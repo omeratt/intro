ac800211018afe8a344eff7f3d78fee570139087
omeratt <omeratt1405@gmail.com>
2022-09-23 17:12:20 +0300

    magic trick

--- js.js
+++ js.js
@@ -1,11 +1,22 @@
-$(document).ready(function () {
-  for (let i = 0; i < 40; i++) {
-    $(".container").append(`<div class="row-${i}"></div>`);
-    for (let j = 0; j < 5; j++) {
-      let width = Math.floor(Math.random() * 10 + 1);
-      $(`.row-${i}`).append(
-        `<div class="box-${width}" style="--width: ${width * 1.2};"></div>`
-      );
-    }
-  }
+const container = document.querySelector(".container");
+const cloneContainer = container.cloneNode(true);
+cloneContainer.classList.add("duplicate");
+container.append(cloneContainer);
+
+container.addEventListener("click", () => {
+  container.classList.add("slide", "no-load");
+  const rows = container.querySelectorAll(".row");
+  const cells = container.querySelectorAll(".cell");
+
+  cells.forEach((cell) => {
+    cell.classList.add("flip");
+  });
+  const lastRow = rows[rows.length - 1];
+  const lastCell = cells[cells.length - 1];
+  lastRow.addEventListener("animationend", () => {
+    cells.forEach((cell) => {
+      cell.classList.remove("flip");
+    });
+    container.classList.remove("slide");
+  });
 });
